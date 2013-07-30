96483d818627f006fb5bc577de29a38d8396cc11
John <master3758@gmail.com>
2013-07-30 21:23:42 +0800

    Update KDTree.cpp

--- KDTree.cpp
+++ KDTree.cpp
@@ -1,3 +1,4 @@
+//KDTree数求KNN，与某一点最近的k个点，优先队列保存最近的k个点，其实就是建一个有k个元素的最小堆
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -12,7 +13,7 @@
 #include <assert.h>
 using namespace std;
 
-typedef double real;
+typedef int real;
 typedef int int32;
 typedef long long int64;
 typedef unsigned int uint32;
@@ -21,6 +22,8 @@ typedef short int16;
 typedef unsigned uint16;
 typedef char int8;
 typedef unsigned char uint8;
+
+const int maxn = 50005;
 
 struct point
 {
@@ -38,25 +41,24 @@ struct point
             else cout << " " << p[i] ;
         cout << endl;
     }
+    friend bool operator == (const point& p, const point & q)
+    {
+        assert(p.dim == q.dim);
+        bool f = true;
+        for(int i = 0; i < p.dim; ++i)
+        {
+            f &= (p.p[i] == q.p[i]);
+        }
+        return f;
+    }
 };
 
-bool operator == (const point& p, const point & q)
-{
-    assert(p.dim == q.dim);
-    bool f = true;
-    for(int i = 0; i < p.dim; ++i)
-    {
-        f &= (p.p[i] == q.p[i]);
-    }
-    return f;
-}
 
 //KDTree
 struct KDTree
 {
     int splitNum;
     point data;
-    bool isLeaf;
     KDTree* left;
     KDTree* right;
     KDTree()
@@ -64,10 +66,11 @@ struct KDTree
         splitNum = -1;
         left = NULL;
         right = NULL;
-        isLeaf = false;
     }
 };
 
+point data[maxn];
+int N;
 template <typename T>
 T power(T p)
 {
@@ -81,12 +84,11 @@ T getMax(T a, T b)
 }
 
 //计算方差
-vector<real> calS(const vector<point>& data)
+vector<real> calS(int l, int r)
 {
-    assert(data.size() > 0);
     int size = data[0].dim;
     vector<real> p(size, 0);
-    for(int i = 0; i < data.size(); ++i)
+    for(int i = l; i < r + 1; ++i)
     {
         for(int j = 0; j < size; ++j)
         {
@@ -96,7 +98,7 @@ vector<real> calS(const vector<point>& data)
     for(int i = 0; i < size; ++i)
         p[i] /= (real)size;
     vector<real> pp(size, 0);
-    for(int i = 0; i < data.size(); ++i)
+    for(int i = l; i < r + 1; ++i)
     {
         for(int j = 0; j < data[i].dim; ++j)
         {
@@ -106,10 +108,10 @@ vector<real> calS(const vector<point>& data)
     return pp;
 }
 
-int select(const vector<point>& data)
+//根据方差选择坐标轴
+int select(int l, int r)
 {
-    assert(data.size() > 0);
-    vector<real> p = calS(data);
+    vector<real> p = calS(l, r);
     int mmax = 0;
     int index = -1;
     for(int i = 0; i < p.size(); ++i)
@@ -123,58 +125,45 @@ int select(const vector<point>& data)
     return index;
 }
 
-KDTree* buildKDTree(KDTree* root, const vector<point>& dataSet)
+void QuickSort(int pos, int first, int last)
 {
-    if(dataSet.size() == 0)
+    int i = first, j = last;
+    point tmp = data[first];
+    while(i < j)
+    {
+        while(i < j && data[j].p[pos] >= tmp.p[pos]) --j;
+        if(i < j) data[i] = data[j];
+        while(i < j && data[i].p[pos] <= tmp.p[pos]) ++i;
+        if(i < j) data[j] = data[i];
+    }
+    data[i] = tmp;
+    if(first < i - 1)
+        QuickSort(pos, first, i-1);
+    if(last > i + 1)
+        QuickSort(pos, i+1, last);
+}
+
+KDTree* buildKDTree(KDTree* root, int l, int r, int dep)
+{
+    if(l > r)
         return NULL;
     root = new KDTree();
-    if(dataSet.size() == 1)
+    if(l == r)
     {
         root->splitNum = 0;
-        root->isLeaf = true;
-        root->data = dataSet[0];
+        root->data = data[l];
         root->left = NULL;
         root->right = NULL;
         return root;
     }
-    vector<point> lt;
-    vector<point> rt;
-    vector<real> pt;
-
-    int index = select(dataSet);
-    for(int i = 0; i < dataSet.size(); ++i)
-    {
-        pt.push_back(dataSet[i].p[index]);
-    }
-    sort(pt.begin(), pt.end());
-    int selectV = pt[pt.size()/2];
-
+    //一种是根据层进行寻找分割轴
+    int index = dep%N;//select(l, r);
+    QuickSort(index, l, r);
+    int mid = l + (r-l)/2;
     root->splitNum = index;
-    int pos = -1;
-    for(int i = 0; i < dataSet.size(); ++i)
-    {
-        if(dataSet[i].p[index] == selectV)
-        {
-            root->data = dataSet[i];
-            pos = i;
-            break;
-        }
-    }
-    for(int i = 0; i < dataSet.size(); ++i)
-    {
-        if(pos == i)
-            continue;
-        if(dataSet[i].p[index] <= selectV)
-        {
-            lt.push_back(dataSet[i]);
-        }
-        else
-        {
-            rt.push_back(dataSet[i]);
-        }
-    }
-    root->left = buildKDTree(root->left, lt);
-    root->right = buildKDTree(root->right, rt);
+    root->data = data[mid];
+    root->left = buildKDTree(root->left, l, mid-1, dep+1);
+    root->right = buildKDTree(root->right, mid+1, r, dep+1);
     return root;
 }
 
@@ -192,10 +181,8 @@ bool operator < (const Node& p, const Node& q)
 
 priority_queue<Node> pque;
 
-
 real pqdis(const point& p, const point& q)
 {
-    assert(p.dim == q.dim);
     real sum = 0;
     for(int i = 0; i < p.dim; ++i)
     {
@@ -204,6 +191,7 @@ real pqdis(const point& p, const point& q)
     return sum;
 }
 
+//遍历该tree树
 void trans(KDTree* root)
 {
     root->data.output();
@@ -231,75 +219,49 @@ T Abs(T a)
     return a > 0 ? a : -a;
 }
 
-void searchTopK(KDTree* root, const point& p, int k)
+void searchTopK(KDTree* root, const point& p, int k, int dep)
 {
     if(root == NULL)
         return ;
-    if(root->isLeaf)
-    {
-        Node node;
-        node.p = root->data;
-        node.dis = pqdis(p, node.p);
-        if(pque.size() < k || pque.top().dis > node.dis)
-        {
-            pque.push(node);
-            if(pque.size() > k)
-            {
-                pque.pop();
-            }
-        }
-        return ;
-    }
-    int split = root->splitNum;
-    //cout << split << endl;
     KDTree* child1 = NULL;
     KDTree* child2 = NULL;
-    if(split >= 0 && p.p[split] <= root->data.p[split])
+    int split = dep%N;//root->splitNum;
+    if(p.p[split] <= root->data.p[split])
     {
         child1 = root->left;
         child2 = root->right;
     }
-    else if(split >= 0)
+    else
     {
         child1 = root->right;
         child2 = root->left;
     }
     if(child1)
     {
-        searchTopK(child1, p, k);
-    }else
-    {
-        Node node;
-        node.p = root->data;
-        node.dis = pqdis(p, node.p);
-        if(split >= 0 && (pque.size() < k || pque.top().dis > node.dis))
-        {
-            pque.push(node);
-            if(pque.size() > k)
-            {
-                pque.pop();
-            }
-        }
+        searchTopK(child1, p, k, dep+1);
     }
-    real d = power(Abs(root->data.p[split]-p.p[split]));
-    if(split >= 0 && (d < pque.top().dis || pque.size() < k))
+    Node node;
+    node.p = root->data;
+    node.dis = pqdis(p, node.p);
+    bool f = false;
+    if(pque.size() < k )
     {
-        if(child2)
-        {
-            searchTopK(child2, p, k);
-        }
-        Node node;
-        node.p = root->data;
-        node.dis = pqdis(p, node.p);
-        if(pque.size() < k || pque.top().dis > node.dis)
-        {
-            pque.push(node);
-            if(pque.size() > k)
-            {
-                pque.pop();
-            }
-        }
+        pque.push(node);
+        f = true;
     }
+    else
+    {
+        if(pque.top().dis > node.dis) {
+            pque.pop();
+            pque.push(node);
+        }
+        real d = power(Abs(root->data.p[split]-p.p[split]));
+        if(d < pque.top().dis)
+            f = true;
+    }
+
+    if(child2 && f)
+        searchTopK(child2, p, k, dep+1);
 }
 
 void findKNN(vector<point>& p)
@@ -311,4 +273,52 @@ void findKNN(vector<point>& p)
         pque.pop();
     }
     std::reverse(p.begin(), p.end());
+}
+
+//hdu4347
+int main()
+{
+    //freopen("1009.in","r",stdin);
+    //freopen("data.out","w",stdout);
+    int m, n; //m组数，n维
+    while(scanf("%d%d", &m, &n)!=EOF)
+    {
+        N = n;
+        while(!pque.empty())
+            pque.pop();
+        for(int i = 0; i < m; ++i)
+        {
+            vector<real> p;
+            real a;
+            for(int j = 0; j < n; ++j)
+            {
+                scanf("%d", &a);
+                p.push_back(a);
+            }
+            data[i] = p;
+        }
+        KDTree* root = NULL;
+        root = buildKDTree(root,0,m-1, 0);
+        int t;
+        scanf("%d", &t);
+        while(t--)
+        {
+            vector<real> p;
+            real a;
+            for(int j = 0; j < n; ++j)
+            {
+                scanf("%d", &a);
+                p.push_back(a);
+            }
+            int K;
+            scanf("%d", &K);
+            searchTopK(root, point(p), K, 0);
+            vector<point> res;
+            findKNN(res);
+            cout << "the closest " << K << " points are:" << endl;
+            for(vector<point>::iterator it = res.begin(); it != res.end(); ++it)
+                (*it).output();
+        }
+    }
+    return 0;
 }
